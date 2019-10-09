42718c78653687a544f64909a5702366e9e73ce0
Yuying He <yuying.he111@gmail.com>
2019-10-09 12:57:20 -0400

    add signup screen

---
+++ screens/SignupScreen.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { Text, View, TextInput, } from 'react-native'
+
+export default class SignupScreen extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text> Signup  </Text>
+                <TextInput placeholder="user name"></TextInput>
+                <TextInput placeholder="password"></TextInput>
+                <TextInput placeholder="verify password"></TextInput>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+    }
+})
